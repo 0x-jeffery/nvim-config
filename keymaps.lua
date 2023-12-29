1e79107da6c5f0f7e8b5d34db2fb19e87ac0f5cc
Jaffreh <jefferyfrederic@gmail.com>
2023-12-29 13:54:14 -0700

    file run feature added

--- keymaps.lua
+++ keymaps.lua
@@ -3,6 +3,7 @@
 -- File saving and quitting maps
 vim.keymap.set('n', '<leader>fs', "<Esc>:w<CR>", {desc = '[F]ile [S]ave'});
 vim.keymap.set('n', '<leader>fq', "<Esc>:q<CR>", {desc = '[F]ile [Q]uit'});
+vim.keymap.set('n', '<leader>fr', "<Esc>:!./run.sh<CR>", {desc = '[F]ile [R]un'});
 vim.keymap.set({ 'n', 'v', 'i'}, '<C-s>', '<Esc>:w<CR>', {desc = 'SAVE'});
 vim.keymap.set({'n', 'v'}, '<leader>q', "<Esc>:q<CR>", {desc = 'QUIT'});
 
